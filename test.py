b602b3983312fc11c55af9af025a20424d1190bb
Agnes <agnes789@gmail.com>
2022-12-18 16:28:38 +0100

    day

--- test.py
+++ test.py
@@ -1,9 +1,24 @@
 from fitparse import FitFile
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.dates import AutoDateFormatter, AutoDateLocator
+import numpy as np
 from statistics import mean
 from scipy.signal import savgol_filter
+import datetime
 
 fit_file = FitFile('8234833773_ACTIVITY.fit')
+
+def my_format_function(x, pos=None):
+    x = mpl.dates.num2date(x)
+    if pos == 0:
+        fmt = '%D %H:%M:%S'
+    else:
+        fmt = '%H:%M:%S.%f'
+    label = x.strftime(fmt)
+    label = label.rstrip("0")
+    label = label.rstrip(".")
+    return label
 
 # for record in fit_file.get_messages("record"):
 #     # Records can contain multiple pieces of data (ex: timestamp, latitude, longitude, etc)
@@ -22,13 +37,14 @@ fit_file = FitFile('8234833773_ACTIVITY.fit')
 #         if data.name == 'unknown_108':
 #             print(f"{data.name}, {data.value}, {data.units}")
 
-
+start_time = 0
 power = []
 power_smoth = []
 temp_smoth = []
 breath = []
 i = 0
 temp_avarage = 0
+x = []
 for record in fit_file.get_messages("record"):
     # Records can contain multiple pieces of data (ex: timestamp, latitude, longitude, etc)
     for data in record:
@@ -47,17 +63,40 @@ for record in fit_file.get_messages("record"):
             power_smoth.append(temp_avarage)
         if data.name == 'unknown_108':
             breath.append(data.value/100)
+        if data.name == 'timestamp':
+            if start_time == 0 :
+                start_time = data.value
+            x.append(data.value-start_time)
+    if len(breath) < len(x) :
+        breath.append(20)
+
 
 yhat = savgol_filter(power, 151, 3) # window size 51, polynomial order 3
 print(len(power))
 print(len(power_smoth))
 print(len(breath))
+print(len(x))
 
 x = []
 for i in range(len(breath)):
   x.append(i)
 
 #plt.plot(x, power, color='green')
-plt.plot(x, breath)
+fig, ax = plt.subplots(figsize=(10, 5.2), layout='constrained')
+ax.set_xlabel('Entry A')
+ax.set_ylabel('Entry B')
+#xtick_locator = AutoDateLocator()
+#xtick_formatter = AutoDateFormatter(xtick_locator)
+def timeTicks(x, pos):
+    d = datetime.timedelta(seconds=x)
+    return str(d)
+xtick_formatter = mpl.ticker.FuncFormatter(timeTicks)
+#xtick_formatter.scaled[1/(24*60)] = my_format_function
+
+#ax.xaxis.set_major_locator(xtick_locator)
+ax.xaxis.set_major_formatter(xtick_formatter)
 #plt.plot(x, yhat, color='red')
+ax.plot(x, breath)
+ax.grid(True)
+
 plt.show()
