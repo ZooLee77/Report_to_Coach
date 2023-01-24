abfc47c485cf523b611c58380d36cf31f2794b42
Agnes <agnes789@gmail.com>
2023-01-24 07:15:34 +0100

    day

--- report_exercise.py
+++ report_exercise.py
@@ -13,21 +13,22 @@ from tabulate import tabulate
 # import pandas as pd
 import read_fit_file_func as readfit
 import get_cloud_data as getc
-import json
-
+#import json
 
 api = None
 relative_effort = ''
 text_weather = ''
+sleep_text = ''
 today = datetime.date.today()
-startdate = today - datetime.timedelta(days=7)
+#startdate = today - datetime.timedelta(days=7)
 x = []
 weight = []
 
-feel_map ={0: "Nagyon gyenge", 25: "Gyenge", 50: "Normál", 75: "Erős", 100: "Nagyon erős"}
-rpe_map ={10: "1/10 - Nagyon könnyű", 20: "2/10 - Könnyű", 30: "3/10 - Mérsékelt", 40: "4/10 - Kissé nehéz",
-          50: "5/10- Nehéz", 60: "6/10- Nehéz", 70: "7/10- Nagyon nehéz", 80: "8/10- Nagyon nehéz", 90: "9/10- Rendkívül nehéz"
-          , 100: "Maximális"}
+feel_map = {0: "Nagyon gyenge", 25: "Gyenge", 50: "Normál", 75: "Erős", 100: "Nagyon erős"}
+rpe_map = {10: "1/10 - Nagyon könnyű", 20: "2/10 - Könnyű", 30: "3/10 - Mérsékelt", 40: "4/10 - Kissé nehéz",
+           50: "5/10- Nehéz", 60: "6/10- Nehéz", 70: "7/10- Nagyon nehéz", 80: "8/10- Nagyon nehéz",
+           90: "9/10- Rendkívül nehéz",
+           100: "Maximális"}
 
 for i in range(10):
     # Init API
@@ -37,25 +38,10 @@ for i in range(10):
         break
 
 activity_id = last_activity["activityId"]
-#activity_id = "10341956145"
-#print(json.dumps(last_activity, indent=4))
+# activity_id = "10341956145"
+# print(json.dumps(last_activity, indent=4))
 fit_file_name = getc.download_activity(api, activity_id)
 
-evaluation = api.get_activity_evaluation(activity_id)
-if "directWorkoutFeel" in evaluation['summaryDTO'].keys():
-    feel = feel_map[evaluation['summaryDTO']['directWorkoutFeel']]
-    rpe = rpe_map[evaluation['summaryDTO']['directWorkoutRpe']]
-    relative_effort = "Hogy érzi magát? " + feel + " Észlelt erőfeszítés: " + rpe
-
-weather = api.get_activity_weather(activity_id)
-if isinstance(weather['temp'], (int, float)):
-    text_weather = "Hömérséklet: " + "{0:.4}".format((weather['temp'] - 32) / 1.8) + "(C) Hőérzet: " + "{0:.4}".format(
-        (150 - 32) / 1.8) + "(C) Páratartalom: " + str(
-        weather['relativeHumidity']) + "% Szélsebesség: " + "{0:.4}".format(weather['windSpeed'] * 1.852) + " kmp"
-
-for data in api.get_body_composition(startdate.isoformat(), today.isoformat())["dateWeightList"]:
-    x.append(datetime.datetime.fromtimestamp(data['date'] / 1000))
-    weight.append(data['weight'] / 1000)
 
 # fit_file_list = ['10153960586_ACTIVITY.fit', '10168131101_ACTIVITY.fit', '10176445328_ACTIVITY.fit',
 #                  '10190995780_ACTIVITY.fit', '10197069218_ACTIVITY.fit', '10207850540_ACTIVITY.fit',
@@ -73,6 +59,114 @@ workout_dic = readfit.read_fit_file_workout(fit_file_name)
 # print(start_time)
 
 filename_prefix = laps_dataframe["Kör kezdő idő"].iloc[0].strftime("%Y_%m_%d_%H_%M_%S")
+
+
+evaluation = api.get_activity_evaluation(activity_id)
+if "directWorkoutFeel" in evaluation['summaryDTO'].keys():
+    feel = feel_map[evaluation['summaryDTO']['directWorkoutFeel']]
+    rpe = rpe_map[evaluation['summaryDTO']['directWorkoutRpe']]
+    relative_effort = "Hogy érzi magát? " + feel + " Észlelt erőfeszítés: " + rpe
+
+weather = api.get_activity_weather(activity_id)
+if isinstance(weather['temp'], (int, float)):
+    text_weather = "Hömérséklet: " + "{0:.4}".format((weather['temp'] - 32) / 1.8) + "(C) Hőérzet: " + "{0:.4}".format(
+        (weather['apparentTemp'] - 32) / 1.8) + "(C) Páratartalom: " + str(
+        weather['relativeHumidity']) + "% Szélsebesség: " + "{0:.4}".format(weather['windSpeed'] * 1.852) + " kmph"
+
+
+def save_weight(api, actualday):
+    x = []
+    filename_prefix = actualday.strftime("%Y_%m_%d")
+    startdate = actualday - datetime.timedelta(days=7)
+    for data in api.get_body_composition(startdate.isoformat(), actualday.isoformat())["dateWeightList"]:
+        x.append(datetime.datetime.fromtimestamp(data['date'] / 1000))
+        weight.append(data['weight'] / 1000)
+    fig, ax = plt.subplots(figsize=(15, 5.2))
+    ax.set_xlabel('Súly')
+    ax.set_ylabel('Dátum')
+    ax.plot(x, weight, label='Súly')
+    plt.legend()
+    plt.savefig(filename_prefix + '_weight.png')
+
+
+def save_sleep(api, date):
+    def format_timedelta(td):
+        minutes, seconds = divmod(td.seconds + td.days * 86400, 60)
+        hours, minutes = divmod(minutes, 60)
+        if hours > 0:
+            return '{:d}:{:02d}:{:02d}'.format(hours, minutes, seconds)
+        else:
+            return '{:02d}:{:02d}'.format(minutes, seconds)
+
+    x = []
+    activityszint = []
+    filename_prefix = date.strftime("%Y_%m_%d")
+    sleep = api.get_sleep_data(date.isoformat())
+    if not sleep['dailySleepDTO']['id']:
+        return ""
+    for data in sleep["sleepLevels"]:
+        x.append(datetime.datetime.fromisoformat(data['startGMT']))
+        x.append(datetime.datetime.fromisoformat(data['endGMT']) - datetime.timedelta(seconds=1))
+        activityszint.append(data['activityLevel'])
+        activityszint.append(data['activityLevel'])
+    eber = np.array([0.9] * len(activityszint))  # 66B2FF
+    rem = np.array([1.9] * len(activityszint))  # 990099
+    ebrenlet = np.array([2.9] * len(activityszint))  # FF66B2
+
+    fig, ax = plt.subplots(figsize=(15, 5.2))
+    ax.set_ylabel('Alvási szakaszok')
+    ax.set_xlabel('Dátum')
+    plt.ylim([-1, max(activityszint)])
+    ax.plot(x, activityszint, label='Alvás')
+    ax.fill_between(x, -1, 0, where=(activityszint < eber), color='#004C99', alpha=0.5)
+    ax.fill_between(x, -1, 1, where=(
+        list(map(lambda x, y: x and y, (activityszint > eber), (activityszint < rem)))), color='#66B2FF', alpha=0.5)
+    ax.fill_between(x, -1, 2, where=(
+        list(map(lambda x, y: x and y, (activityszint > rem), (activityszint < ebrenlet)))), color='#990099', alpha=0.5)
+    ax.fill_between(x, -1, 3, where=(activityszint > ebrenlet), color='#FF66B2', alpha=0.5)
+    plt.xlim(x[0], x[-1])
+    start_value = x[0].strftime("%m-%d %H:%M")
+    end_value = x[-1].strftime("%m-%d %H:%M")
+    ax.annotate(f"Start: {start_value}", xy=(ax.get_xlim()[0], -1), xycoords='data', xytext=(-50, -30),
+                textcoords='offset points', arrowprops=dict(arrowstyle="->",
+                                                            connectionstyle="arc3,rad=.2"))
+    ax.annotate(f"End: {end_value}", xy=(ax.get_xlim()[1], -1), xycoords='data', xytext=(-50, -30),
+                textcoords='offset points', arrowprops=dict(arrowstyle="->",
+                                                            connectionstyle="arc3,rad=.2"))
+    ax.set_yticks([0, 1, 2, 3])
+    plt.yticks(ticks=[0, 1, 2, 3], labels=["Mély", "Éber", "REM", "Ébrenlét"], fontsize=12)
+    plt.savefig(filename_prefix + '_sleep.png')
+    sleep_text = ""
+    sleep_text += "Alvási adatok: " + str(sleep['dailySleepDTO']['calendarDate']) + "\n"
+    sleep_text += "Alvási eredmény " + str(sleep['dailySleepDTO']['sleepScores']["overall"]['value']) + " /100\n"
+    sleep_text += "Az alvás egésze " + sleep['dailySleepDTO']['sleepScores']["overall"]['qualifierKey'] + "\n"
+    sleep_text += "Alvásról visszajelzés " + sleep['dailySleepDTO']['sleepScoreFeedback'] + "\n"
+    sleep_text += "Teljes hossz " + format_timedelta(
+        datetime.timedelta(seconds=sleep['dailySleepDTO']['sleepTimeSeconds']))
+    sleep_text += " " + sleep['dailySleepDTO']['sleepScores']["totalDuration"]['qualifierKey'] + "\n"
+    sleep_text += "Alvási stress " + str(sleep['dailySleepDTO']['avgSleepStress'])
+    sleep_text += " " + sleep['dailySleepDTO']['sleepScores']["stress"]['qualifierKey'] + "\n"
+    sleep_text += "Mély alvás " + format_timedelta(
+        datetime.timedelta(seconds=sleep['dailySleepDTO']['deepSleepSeconds']))
+    sleep_text += " " + sleep['dailySleepDTO']['sleepScores']["deepPercentage"]['qualifierKey'] + "\n"
+    sleep_text += "Könnyű alvás " + format_timedelta(
+        datetime.timedelta(seconds=sleep['dailySleepDTO']['lightSleepSeconds']))
+    sleep_text += " " + sleep['dailySleepDTO']['sleepScores']["lightPercentage"]['qualifierKey'] + "\n"
+    sleep_text += "REM " + format_timedelta(datetime.timedelta(seconds=sleep['dailySleepDTO']['remSleepSeconds']))
+    sleep_text += " " + sleep['dailySleepDTO']['sleepScores']["remPercentage"]['qualifierKey'] + "\n"
+    sleep_text += "Ébrenlét " + format_timedelta(
+        datetime.timedelta(seconds=sleep['dailySleepDTO']['awakeSleepSeconds']))
+    sleep_text += " " + sleep['dailySleepDTO']['sleepScores']["awakeCount"]['qualifierKey'] + "\n"
+    sleep_text += "(**********************) " + "\n"
+    return sleep_text
+
+
+save_weight(api, today)
+
+current_date = datetime.datetime.fromisoformat(last_activity['startTimeGMT']).date()
+while current_date <= today:
+    sleep_text += save_sleep(api, current_date)
+    current_date += datetime.timedelta(days=1)
 
 laps_dataframe.insert(laps_dataframe.columns.get_loc("Max. pulzus") + 1, 'Min. pulzus', np.nan)
 for e in range(len(laps_dataframe.index)):
@@ -197,7 +291,7 @@ Workout Speed in the target zone: {SpeedPercent}
 {Weather}
 
 {table}
-
+{sleep}
 Regards,
 
 Me"""
@@ -217,22 +311,22 @@ html = """
 <p>Workout Speed in the target zone: {SpeedPercent}</p>
 <p>{Relative_effort}</p>
 <p>{Weather}</p>
-
 {table}
+<p>{sleep}</p>
 <p>Regards,</p>
 <p>Me</p>
 </body></html>
 """
-text = text.format(table=tabulate(laps_dataframe, headers="keys", tablefmt="grid")
-                   , WorkoutName=workout_dic['Workout_Name']
-                   , HRPercent="{0:.2%}".format(workout_dic['HR percent'])
-                   , SpeedPercent="{0:.2%}".format(workout_dic['Speed percent'])
-                   , Relative_effort=relative_effort, Weather=text_weather)
-html = html.format(table=tabulate(laps_dataframe, headers="keys", tablefmt="html")
-                   , WorkoutName=workout_dic['Workout_Name']
-                   , HRPercent="{0:.2%}".format(workout_dic['HR percent'])
-                   , SpeedPercent="{0:.2%}".format(workout_dic['Speed percent'])
-                   , Relative_effort=relative_effort, Weather=text_weather)
+text = text.format(table=tabulate(laps_dataframe, headers="keys", tablefmt="grid"),
+                   WorkoutName=workout_dic['Workout_Name'],
+                   HRPercent="{0:.2%}".format(workout_dic['HR percent']),
+                   SpeedPercent="{0:.2%}".format(workout_dic['Speed percent']),
+                   Relative_effort=relative_effort, Weather=text_weather, sleep=sleep_text)
+html = html.format(table=tabulate(laps_dataframe, headers="keys", tablefmt="html"),
+                   WorkoutName=workout_dic['Workout_Name'],
+                   HRPercent="{0:.2%}".format(workout_dic['HR percent']),
+                   SpeedPercent="{0:.2%}".format(workout_dic['Speed percent']),
+                   Relative_effort=relative_effort, Weather=text_weather, sleep=sleep_text)
 
 f = open(filename_prefix + '_data.html', 'w')
 f.write(html)
@@ -241,13 +335,6 @@ f.close()
 f = open(filename_prefix + '_data.txt', 'w')
 f.write(text)
 f.close()
-
-fig, ax = plt.subplots(figsize=(15, 5.2))
-ax.set_xlabel('Súly')
-ax.set_ylabel('Dátum')
-ax.plot(x, weight, label='Súly')
-plt.legend()
-plt.savefig(filename_prefix + '_weight.png')
 
 
 def plotter_dict(first, second=None, third=None):
@@ -337,4 +424,3 @@ if not np.isnan(altitude_dict['data'].iloc[-1]):
     if not np.isnan(gear_ratio_dict['data'].iloc[-1]):
         plotter_dict(cadence_dict, gear_ratio_dict, altitude_dict)
     plotter_dict(cadence_dict, speed_dict, altitude_dict)
-
